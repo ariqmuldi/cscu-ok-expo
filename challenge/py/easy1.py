7f4de367c989b27bcfee8d6c903b776707a745e3
Ariq Muldi <ariqmuldi@gmail.com>
2024-09-18 11:35:31 -0700

    comment

--- challenge/py/easy1.py
+++ challenge/py/easy1.py
@@ -1,7 +1,7 @@
 def print_even_numbers(numbers):
     for num in numbers:
         if num % 2 == 1:
-            print("{num} is even")
+            print("{num} is even") # Make it print the even number!
             
 numbers = [1, 2, 3, 4, 5, 6]
 print_even_numbers(numbers)
